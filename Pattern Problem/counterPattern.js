1b428cdecd5c0a069412dda5eaa8068defc3759c
Abhishek9607 <abhi7549ku@gmail.com>
2023-10-10 21:20:29 +0530

    Counter Pattern

---
+++ Pattern Problem/counterPattern.js
@@ -0,0 +1,17 @@
+function pattern(n) {
+    for(let row = 1; row <= n; row++) {
+        let str = "";
+        let spaces = n- row;
+        for(let i = 1; i <= spaces; i++) {
+            str += " ";
+        }
+        let counter = 1;
+        for(let j = 1; j <= row; j++) {
+            str += counter;
+            counter++
+        }
+        console.log(str);
+    }
+}
+
+pattern(8);
